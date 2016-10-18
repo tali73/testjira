c2d474ea7abea72c11130acf1e1705655bb74982
tali73 <tali73@gmail.com>
2016-10-18 14:53:39 +0300

    ' ;

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,10 +10,11 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            string s1 = "first", s2 = "second", s3 = "third";
+            string s1 = "first", s2 = "second", s3 = "third", s4="fourth";
             Console.WriteLine(s1);
             Console.WriteLine(s2);
             Console.WriteLine(s3);
+            Console.WriteLine(s4);
         }
     }
 }
