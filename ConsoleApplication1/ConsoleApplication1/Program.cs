13556a5aa7ecc3248225ce74f0b31df9e860c201
tali73 <tali73@gmail.com>
2016-10-18 14:03:17 +0300

    commit 1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "first", s2 = "second";
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
